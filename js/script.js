6a54ebcfa23bbd91cd111f9ba0647895b290203b
AnnaSenchukova <36882792+AnnaSenchukova@users.noreply.github.com>
2018-07-11 11:27:41 +0300

    Финальная проверка. Д14. Все скрипты собраны в один файл.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
+function initMap() {
+  var coordinates = new google.maps.LatLng(59.939130, 30.321476);
+  var myMap = {
+    zoom: 17,
+    center: coordinates
+  };
+
+  var map = new google.maps.Map(document.querySelector('.map__image'), myMap);
+
+  var image = 'img/map_pointer.png';
+
+  var marker = new google.maps.Marker({
+    map: map,
+    position: new google.maps.LatLng(59.938723, 30.323785),
+    icon: image,
+    title: 'Большая Конюшенная, д.19/8'
+  });
+}
+
+
+
+
 var popup = document.querySelector('.popup');
 var buttonOpenedPopup = document.querySelector('.contacts__button');
 var buttonClosedPopup = popup.querySelector('.popup__button-close');
